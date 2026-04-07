89d15a40aa7bf4f73f951de09d1c744a64e07371
Hidayat3118 <muhammadhidayat3118@gmail.com>
2026-04-07 18:24:52 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true } 
+  images: { unoptimized: true },
+  transpilePackages: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
 };
 
 module.exports = nextConfig;
